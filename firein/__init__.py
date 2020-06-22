89fc1d37f5107e9996e3834e0543a361e25aece2
kerwin <2509456238@qq.com>
2020-06-22 15:32:09 +0800

    add sqlac

--- firein/__init__.py
+++ firein/__init__.py
@@ -5,10 +5,14 @@
 import os
 
 from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
 
 def create_app():
     # create and configure the app
     app = Flask(__name__)
+    app = db.init_app(app)
 
     from .data import databp
     app.register_blueprint(databp,url_prefix='/databp')
