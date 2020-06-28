0af7996ad77fcfbef89864c830c6482afafea3dc
kerwin <2509456238@qq.com>
2020-06-28 14:23:51 +0800

    Annotate db

--- firein/__init__.py
+++ firein/__init__.py
@@ -12,7 +12,7 @@ db = SQLAlchemy()
 def create_app():
     # create and configure the app
     app = Flask(__name__)
-    app = db.init_app(app)
+    # app = db.init_app(app)
 
     from .data import databp
     app.register_blueprint(databp,url_prefix='/databp')
